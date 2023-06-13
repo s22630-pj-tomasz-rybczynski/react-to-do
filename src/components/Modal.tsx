6fe9f3adf02952eb7024c046204505f76ebe0ab9
clarzte <senvill412@gmail.com>
2023-06-13 22:28:15 +0200

    Change form to div

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,15 +1,16 @@
-import React from "react";
+import React, { FormEventHandler } from "react";
 
 interface ModalProps {
     modalOpen: boolean;
     setModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
     children: React.ReactNode;
+    handleSubmit: FormEventHandler<HTMLFormElement>;
 }
 
-const Modal: React.FC<ModalProps> = ({modalOpen, setModalOpen, children}) => {
+const Modal: React.FC<ModalProps> = ({modalOpen, setModalOpen, children, handleSubmit}) => {
     return (
         <dialog id="my_modal_3" className={`modal ${modalOpen ? "modal-open" : ""}`}>
-        <form method="dialog" className="modal-box">
+        <form method="dialog" className="modal-box" onSubmit={handleSubmit}>
             <button onClick={() => setModalOpen(false)} className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
             {children}
         </form>
