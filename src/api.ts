53e3143c9f8470779270579fbda23bdc41ab50e6
clarzte <senvill412@gmail.com>
2023-06-14 19:20:29 +0200

    Add new api functions

--- src/api.ts
+++ src/api.ts
@@ -20,3 +20,24 @@ export const addTodo = async (todo: ITask): Promise<ITask> => {
 
     return await res.json();
 };
+
+export const editTodo = async (todo: ITask): Promise<ITask> => {
+    const res = await fetch(`${baseUrl}/tasks/${todo.id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(todo)
+        }
+    );
+
+    return await res.json();
+};
+
+export const deleteTodo = async (id: string): Promise<void> => {
+    await fetch(`${baseUrl}/tasks/${id}`, {
+        method: 'DELETE'
+        }
+    );
+};
+
