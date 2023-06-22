d45c1290e13efb37e544304cb94736e40163ca2f
clarzte <senvill412@gmail.com>
2023-06-22 21:35:12 +0200

    Add nicer add task

--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -1,4 +1,4 @@
-import {AiOutlinePlus} from 'react-icons/ai'
+import { AiOutlinePlus } from 'react-icons/ai'
 import Modal from './Modal'
 import { FormEventHandler, useState } from 'react'
 import { addTodo } from '../api'
@@ -7,68 +7,86 @@ import { v4 as uuidv4 } from 'uuid'
 import CustomDatePicker from './DatePicker'
 
 interface AddTaskProps {
-    refresh: () => void
+  refresh: () => void
 }
 
-const AddTask: React.FC<AddTaskProps>  = ({refresh}) => {
-    const [modalOpen, setModalOpen] = useState(false)
-    const [newTaskValue, setNewTaskValue] = useState('')
-    const [priority, setPriority] = useState<Priority>(Priority.MEDIUM)
-    const [date, setDate] = useState<Date | null>(new Date())
+const AddTask: React.FC<AddTaskProps> = ({ refresh }) => {
+  const [modalOpen, setModalOpen] = useState(false)
+  const [newTaskValue, setNewTaskValue] = useState('')
+  const [priority, setPriority] = useState<Priority>(Priority.MEDIUM)
+  const [date, setDate] = useState<Date | null>(new Date())
 
-    const handleSubmitNewTodo: FormEventHandler<HTMLFormElement> = async e => {
-        e.preventDefault()
-        if(newTaskValue === '') return
-        await addTodo({
-            id: uuidv4(),
-            text: newTaskValue,
-            priority: priority,
-            done: false,
-            deadline: date ? date : new Date()
-        })
-        setNewTaskValue('')
-        setModalOpen(false)
-        refresh()
-    };
-    
-    return (
-        <div>
-          <button
-            onClick={() => setModalOpen(true)}
-            className="btn btn-primary w-full"
-          >
-            Add new task <AiOutlinePlus size={18} className="ml-2" />
-          </button>
-          <Modal
-            modalOpen={modalOpen}
-            setModalOpen={setModalOpen}
-            handleSubmit={handleSubmitNewTodo}
-          >
-            <h3 className="font-bold text-lg">Add new task</h3>
-            <div className="modal-action">
-                <input
-                    value={newTaskValue}
-                    onChange={(e) => setNewTaskValue(e.target.value)}
-                    type="text"
-                    placeholder="Type here"
-                    className="input input-bordered w-full"
-                />
-                <input type="radio" name="priority" className="radio radio-success" onClick={() => setPriority(Priority.LOW)} />
-                <input type="radio" name="priority" className="radio radio-warning" onClick={() => setPriority(Priority.MEDIUM)} />
-                <input type="radio" name="priority" className="radio radio-error" onClick={() => setPriority(Priority.HIGH)} />
-              <button type="submit" className="btn">
-                Submit
-              </button>
-            </div>
-            <div className="modal-action">
-            <CustomDatePicker
-                selectedDate={date}
-                onChange={(date) => setDate(date)}
+  const handleSubmitNewTodo: FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault()
+    if (newTaskValue === '') return
+    await addTodo({
+      id: uuidv4(),
+      text: newTaskValue,
+      priority: priority,
+      done: false,
+      deadline: date ? date : new Date(),
+    })
+    setNewTaskValue('')
+    setModalOpen(false)
+    refresh()
+  }
+
+  return (
+    <div>
+      <button
+        onClick={() => setModalOpen(true)}
+        className="btn btn-primary w-full"
+      >
+        Add new task <AiOutlinePlus size={18} className="ml-2" />
+      </button>
+      <Modal
+        modalOpen={modalOpen}
+        setModalOpen={setModalOpen}
+        handleSubmit={handleSubmitNewTodo}
+      >
+        <h3 className="font-bold text-lg">Add new task</h3>
+        <div className="modal-action flex flex-col">
+          <div className="flex items-center gap-3">
+            <input
+              value={newTaskValue}
+              onChange={(e) => setNewTaskValue(e.target.value)}
+              type="text"
+              placeholder="Type here"
+              className="input input-bordered w-full"
             />
-            </div>
-          </Modal>
+            <CustomDatePicker
+              selectedDate={date}
+              onChange={(date) => setDate(date)}
+            />
+          </div>
+          <div className="flex items-center gap-2 mt-4">
+            <input
+              type="radio"
+              name="priority"
+              className="radio radio-success"
+              onClick={() => setPriority(Priority.LOW)}
+            />
+            <input
+              type="radio"
+              name="priority"
+              className="radio radio-warning"
+              onClick={() => setPriority(Priority.MEDIUM)}
+            />
+            <input
+              type="radio"
+              name="priority"
+              className="radio radio-error"
+              onClick={() => setPriority(Priority.HIGH)}
+            />
+            <button type="submit" className="btn">
+              Submit
+            </button>
+          </div>
         </div>
-      )
-    }    
+        <div className="modal-action"></div>
+      </Modal>
+    </div>
+  )
+}
 
-export default AddTask;
+export default AddTask
